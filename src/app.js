fda10f4fe8fdee8339125918c425d1d374ba2315
柏海辉 <baihaihui@bohaihuideMacBook-Pro.local>
2018-01-19 16:49:58 +0800

    feature:koa-router的引入

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import Koa from 'koa';
+import Router from 'koa-router';
 
 const app = new Koa();
+const router = new Router();
 
+
+router.get('/', (ctx) => {
+  ctx.body = {
+    name: 'hello oneMovie'
+  }
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 
 export default app;
