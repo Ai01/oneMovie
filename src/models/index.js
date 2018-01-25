80e95e6dc18f104ed7c7b06dfc87492e3558e61d
柏海辉 <baihaihui@bohaihuideMacBook-Pro.local>
2018-01-25 15:32:08 +0800

    chore:clean todo

--- src/models/index.js
+++ src/models/index.js
@@ -6,8 +6,6 @@ import UserModel from './user/userModel';
 
 import UserRoleModel from './userRole/userRoleModel';
 
-// TODO:bai 将表的关系放在这里是需要所有的表都建立了。这个理解是正确的吗？
-
 RoleModel.belongsToMany(UserModel, {
   through: UserRoleModel,
   constraints: false,
@@ -17,7 +15,6 @@ UserModel.belongsToMany(RoleModel, {
   through: UserRoleModel,
   constraints: false,
 });
-
 
 export {
   UserModel,
